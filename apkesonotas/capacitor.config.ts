db8fb8ae8c85a89b3cd6639841e72eeef8ad1065
Sebantonio <sebantonio@gmail.com>
2026-05-15 16:56:14 +0200

    -

--- apkesonotas/capacitor.config.ts
+++ apkesonotas/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.apkesonotas',
   appName: 'apkesonotas',
-  webDir: 'www'
+  webDir: 'out'
 };
 
 export default config;
